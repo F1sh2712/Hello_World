940c59247386867b68dd01a19d5e907a4c914762
F1sh2712 <138188133+F1sh2712@users.noreply.github.com>
2023-10-26 22:11:58 +0800

    django setting, run server

--- Django_Projects/sales_management_system/sales_management_system/settings.py
+++ Django_Projects/sales_management_system/sales_management_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p+wo6-w$07#0mbn9m_6l@(uq7#03c4i6o+zn0on#k0ar)l-q!j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 
 # Application definition
